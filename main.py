61a96002c221e5bd66316acd0789fc012a102b74
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-10-17 16:28:37 +0200

    refactor: use clean_sample in train validate test split.

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
                 parameters={
-                    "input": "sample.csv",
+                    "input": "clean_sample.csv:latest",
                     "test_size": config['modeling']['test_size'],
                     "random_seed": config['modeling']['random_seed'],
                     "stratify_by": config['modeling']['stratify_by']
